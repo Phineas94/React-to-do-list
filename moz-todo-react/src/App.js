5aa226b4fc597206d232d9af6d64841196070605
Panashe Tapera <panashetapera94@gmail.com>
2022-02-01 11:43:32 +0200

    Fixed the blank screen error

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,7 +1,5 @@
-
-import './index.css';
-import React from 'react'; 
-import Todo from './components/Todo';
+import logo from './logo.svg';
+import './App.css';
 
 function App(props) {
   return (
@@ -42,22 +40,64 @@ function App(props) {
         </button>
       </div>
       <h2 id="list-heading">
-        3 tasks remaining    
+        3 tasks remaining
       </h2>
-      
-      <ul    
-       role="list"
-        className="todo-list stack-large stack-exception"      
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-       <Todo name="Eat" />
-       <Todo name="Sleep" />
-       <Todo name="Repeat" />
-
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked={true} />
+            <label className="todo-label" htmlFor="todo-0">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-1" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-1">
+              Sleep
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Sleep</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Sleep</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-2" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-2">
+              Repeat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Repeat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Repeat</span>
+            </button>
+          </div>
+        </li>
       </ul>
     </div>
   );
 }
 
-export default App; 
-
+export default App;
